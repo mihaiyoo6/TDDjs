0956cdb91f6063887bdbb55df1bc2307282b8356
Mijea Mihai <mmihai@suitsupply.com>
2016-03-08 09:00:18 +0100

    add mock ajax

--- 04/spec/employee.js
+++ 04/spec/employee.js
@@ -259,3 +259,46 @@ describe('Using mock-ajax for Asynchronous operations testing', function(){
 		expect(successFunction).toHaveBeenCalledWith('Temp 25 C, Sunlight');
 	});
 });
+
+
+describe("Nested Suites - top suite", function(){
+	var count = 0;
+	beforeEach(function(){
+		console.log("Count in top suite: "+count);
+		count++;
+	});
+	afterEach(function(){
+		console.log("Calling top afterEach");
+	});
+	it("Increases count", function(){
+		console.log("Count : " +count);
+		expect(true).toEqual(true);
+	});
+	describe("Nested Suites - Inner suite", function(){
+		beforeEach(function(){
+			console.log("Count in Inner suite: "+count);
+			count++;
+		});
+		afterEach(function(){
+			console.log("Calling inner afterEach");
+		});
+		it("Increases count", function(){
+			console.log("Count : " +count);
+			expect(true).toEqual(true);
+		});
+		describe("Nested Suites - inner most suite", function(){
+			beforeEach(function(){
+				console.log("Count in inner most suite: "+count);
+				count++;
+			});
+			afterEach(function(){
+				console.log("Calling inner most afterEach");
+			});
+			it("Increases count", function(){
+				console.log("Count : " +count);
+				expect(true).toEqual(true);
+			});
+		});
+	});
+});
+
